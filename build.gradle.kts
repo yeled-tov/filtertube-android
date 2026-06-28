2433a91ede76599f446bf5bfcd79b94c754bb742
yeled-tov <ywldyld@gmail.com>
2026-06-28 14:46:20 +0300

    Fix Gradle plugin config for Firebase build

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
